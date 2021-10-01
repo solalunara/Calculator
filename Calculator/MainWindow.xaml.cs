ce65b403c2b64989039c716b03633f000c04a898
solregem <greenberg04@gmail.com>
2021-10-01 13:58:47 -0700

    a

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -35,14 +35,6 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private void NumButton( object sender, RoutedEventArgs e )
-        {
-            if ( sender is Button btn )
-            {
-                Calc.Text += btn.Name[ 1.. ];
-            }
-        }
-
         [Flags]
         private enum FiveFuncs
         {
@@ -51,11 +43,6 @@ namespace Calculator
             DIVIDE      = 1 << 2,
             PLUS        = 1 << 3,
             MINUS       = 1 << 4,
-        }
-        [Flags]
-        private enum MathFuncs
-        {
-            SQRT        = 1 << 0,
         }
 
         private void MetaFunc( object sender, RoutedEventArgs e )
@@ -73,12 +60,12 @@ namespace Calculator
                         catch ( InvalidInputException ex )
                         {
                             Calc.Text = "Invalid Input";
-                            _ = MessageBox.Show( ex.StackTrace, ex.Message );
+                            MessageBox.Show( ex.StackTrace, ex.Message );
                         }
                         catch ( ArgumentOutOfRangeException ex )
                         {
                             Calc.Text = "Please check your parenthases";
-                            _ = MessageBox.Show( ex.StackTrace, ex.Message );
+                            MessageBox.Show( ex.StackTrace, ex.Message );
                         }
                         break;
                     }
@@ -96,7 +83,30 @@ namespace Calculator
             }
         }
 
-        private float EvaluateString( string Expression )
+        private static (int, int) FindPerimPair( string Expression, int StartIndex )
+        {
+            int StartPerimIndex = Expression.IndexOf( '(', StartIndex );
+            int LeftPerims = 0;
+            int RightPerims = 0;
+            int EndPerimIndex = 0;
+            for ( int j = StartPerimIndex; j < Expression.Length; ++j )
+            {
+                if ( Expression[ j ] == '(' )
+                    ++LeftPerims;
+                if ( Expression[ j ] == ')' )
+                    ++RightPerims;
+
+                if ( LeftPerims == RightPerims )
+                {
+                    EndPerimIndex = j;
+                    break;
+                }
+            }
+            Assert( EndPerimIndex != 0 );
+            return (StartPerimIndex, EndPerimIndex);
+        }
+
+        private static float EvaluateString( string Expression )
         {
 
             //evaluate all functions that aren't one of the main 5 first, and turn them into numbers
@@ -108,27 +118,19 @@ namespace Calculator
                     if ( i > 0 && Expression[ i - 1 ] is not '*' and not '/' and not '+' and not '-' and not '^' )
                         Expression = Expression.Insert( i, "*" );
 
-                    //has to be indexof because the above statement may shift it to being i + 5
-                    int StartPerimIndex = Expression.IndexOf( '(', i + 4 );
-                    int LeftPerims = 0;
-                    int RightPerims = 0;
-                    int EndPerimIndex = 0;
-                    for ( int j = StartPerimIndex; j < Expression.Length; ++j )
-                    {
-                        if ( Expression[ j ] == '(' )
-                            ++LeftPerims;
-                        if ( Expression[ j ] == ')' )
-                            ++RightPerims;
+                    (int, int) PerimPair = FindPerimPair( Expression, i );
+                    float EnclosedValue = EvaluateString( Expression[ ( PerimPair.Item1 + 1 )..PerimPair.Item2 ] );
+                    Expression = Expression[ 0..( PerimPair.Item1 - 4 ) ] + MathF.Sqrt( EnclosedValue ).ToString( CultureInfo.CurrentCulture ) + Expression[ ( PerimPair.Item2 + 1 ).. ];
+                }
+                if ( i < Expression.Length - 4 && Expression[ i..( i + 2 ) ] == "ln" )
+                {
+                    //implicit multiplication
+                    if ( i > 0 && Expression[ i - 1 ] is not '*' and not '/' and not '+' and not '-' and not '^' )
+                        Expression = Expression.Insert( i, "*" );
 
-                        if ( LeftPerims == RightPerims )
-                        {
-                            EndPerimIndex = j;
-                            break;
-                        }
-                    }
-                    Assert( EndPerimIndex != 0 );
-                    float EnclosedValue = EvaluateString( Expression[ ( StartPerimIndex + 1 )..EndPerimIndex ] );
-                    Expression = Expression[ 0..( StartPerimIndex - 4 ) ] + MathF.Sqrt( EnclosedValue ).ToString( CultureInfo.CurrentCulture ) + Expression[ ( EndPerimIndex + 1 ).. ];
+                    (int, int) PerimPair = FindPerimPair( Expression, i );
+                    float EnclosedValue = EvaluateString( Expression[ ( PerimPair.Item1 + 1 )..PerimPair.Item2 ] );
+                    Expression = Expression[ 0..( PerimPair.Item1 - 2 ) ] + MathF.Log( EnclosedValue ).ToString( CultureInfo.CurrentCulture ) + Expression[ ( PerimPair.Item2 + 1 ).. ];
                 }
             }
 
@@ -180,28 +182,11 @@ namespace Calculator
             //while loop for parallel parenthases like "(2+3)*(4+5)"
             while ( Expression.Contains( '(' ) || Expression.Contains( ')' ) )
             {
-                int StartPerimIndex = Expression.IndexOf( '(' );
-                int LeftPerims = 0;
-                int RightPerims = 0;
-                int EndPerimIndex = 0;
-                for ( int i = StartPerimIndex; i < Expression.Length; ++i )
-                {
-                    if ( Expression[ i ] == '(' )
-                        ++LeftPerims;
-                    if ( Expression[ i ] == ')' )
-                        ++RightPerims;
-
-                    if ( LeftPerims == RightPerims )
-                    {
-                        EndPerimIndex = i;
-                        break;
-                    }
-                }
-                Assert( EndPerimIndex != 0 );
-                string EnclosedSectionString = Expression[ (StartPerimIndex + 1)..EndPerimIndex ];
+                (int, int) PerimPair = FindPerimPair( Expression, Expression.IndexOf( '(' ) );
+                string EnclosedSectionString = Expression[ (PerimPair.Item1 + 1)..PerimPair.Item2 ];
                 //condense it down to a single number represented as a string
                 string EnclosedSectionValue = EvaluateString( EnclosedSectionString ).ToString( CultureInfo.CurrentCulture );
-                Expression = Expression[ 0..StartPerimIndex ] + EnclosedSectionValue + Expression[ ( EndPerimIndex + 1 ).. ];
+                Expression = Expression[ 0..PerimPair.Item1 ] + EnclosedSectionValue + Expression[ ( PerimPair.Item2 + 1 ).. ];
             }
 
             
@@ -253,7 +238,7 @@ namespace Calculator
 
             //turn the long string into values
             int ValueCount = FiveFunctionList.Count + 1;
-            List<(float, int)> Values = new( ValueCount );
+            List<float> Values = new( ValueCount );
             int StringIndex = 0;
             for ( int i = 0; i < ValueCount; ++i )
             {
@@ -264,7 +249,7 @@ namespace Calculator
 
                 string StringValue = Expression[ StringIndex..StringNextIndex ];
                 if ( float.TryParse( StringValue, out float f ) )
-                    Values.Add( (f, StringIndex) );
+                    Values.Add( f );
                 else
                     throw new InvalidInputException( "Couldn't parse to float" );
 
@@ -287,31 +272,31 @@ namespace Calculator
                         {
                             case FiveFuncs.PWR:
                             {
-                                Values[ j ] = (MathF.Pow( Values[ j ].Item1, Values[ j + 1 ].Item1 ), Values[ j ].Item2);
+                                Values[ j ] = MathF.Pow( Values[ j ], Values[ j + 1 ] );
                                 Values.RemoveAt( j + 1 );
                                 break;
                             }
                             case FiveFuncs.MULTIPLY:
                             {
-                                Values[ j ] = (Values[ j ].Item1 * Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values[ j ] *= Values[ j + 1 ];
                                 Values.RemoveAt( j + 1 );
                                 break;
                             }
                             case FiveFuncs.DIVIDE:
                             {
-                                Values[ j ] = (Values[ j ].Item1 / Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values[ j ] /= Values[ j + 1 ];
                                 Values.RemoveAt( j + 1 );
                                 break;
                             }
                             case FiveFuncs.PLUS:
                             {
-                                Values[ j ] = (Values[ j ].Item1 + Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values[ j ] += Values[ j + 1 ];
                                 Values.RemoveAt( j + 1 );
                                 break;
                             }
                             case FiveFuncs.MINUS:
                             {
-                                Values[ j ] = (Values[ j ].Item1 - Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values[ j ] -= Values[ j + 1 ];
                                 Values.RemoveAt( j + 1 );
                                 break;
                             }
@@ -327,66 +312,15 @@ namespace Calculator
             }
 
             Assert( Values.Count == 1 );
-            return Values[ 0 ].Item1;
+            return Values[ 0 ];
         }
 
         private void FuncButton( object sender, RoutedEventArgs e )
         {
             if ( sender is Button btn )
             {
-                switch ( btn.Name )
-                {
-                    case "pls":
-                    {
-                        Calc.Text += "+";
-                        break;
-                    }
-                    case "sbt":
-                    {
-                        Calc.Text += "-";
-                        break;
-                    }
-                    case "mult":
-                    {
-                        Calc.Text += "*";
-                        break;
-                    }
-                    case "div":
-                    {
-                        Calc.Text += "/";
-                        break;
-                    }
-                    case "dot":
-                    {
-                        Calc.Text += ".";
-                        break;
-                    }
-                    case "leftperim":
-                    {
-                        Calc.Text += "(";
-                        break;
-                    }
-                    case "rightperim":
-                    {
-                        Calc.Text += ")";
-                        break;
-                    }
-                    case "sqrt":
-                    {
-                        Calc.Text += "sqrt(";
-                        break;
-                    }
-                    case "pwr":
-                    {
-                        Calc.Text += "^";
-                        break;
-                    }
-                    default:
-                    {
-                        Assert( false, "Button not in table!" );
-                        break;
-                    }
-                }
+                Assert( btn.HasContent );
+                Calc.Text += btn.Content;
             }
         }
 
