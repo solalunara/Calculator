2297dae563ab8b0e0fe3bc236142bc8a043b4b74
solregem <greenberg04@gmail.com>
2021-09-30 19:38:36 -0700

    s

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
 using static System.Diagnostics.Debug;
 
 namespace Calculator
@@ -28,8 +29,6 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static int CurrentIndex = 1;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -39,18 +38,17 @@ namespace Calculator
         {
             if ( sender is Button btn )
             {
-                Calc.Text = Calc.Text.Insert( CurrentIndex, btn.Name[ 1.. ] );
-                CurrentIndex += btn.Name.Length - 1;
+                Calc.Text += btn.Name[ 1.. ];
             }
         }
 
+        [Flags]
         private enum MathFuncs
         {
-            NONE        = 0,
-            PLUS        = 1 << 0,
-            MINUS       = 1 << 1,
-            MULTIPLY    = 1 << 2,
-            DIVIDE      = 1 << 3,
+            MULTIPLY    = 1 << 0,
+            DIVIDE      = 1 << 1,
+            PLUS        = 1 << 2,
+            MINUS       = 1 << 3,
         }
 
         private void MetaFunc( object sender, RoutedEventArgs e )
@@ -63,122 +61,23 @@ namespace Calculator
                     {
                         try
                         {
-                            string Expression = Calc.Text;
-                            List<(MathFuncs, int)> FunctionList = new();
-                            for ( int i = 0; i < Expression.Length; ++i )
-                            {
-                                switch ( Expression[ i ] )
-                                {
-                                    case '/':
-                                    {
-                                        FunctionList.Add( (MathFuncs.DIVIDE, i) );
-                                        break;
-                                    }
-                                    case '*':
-                                    {
-                                        FunctionList.Add( (MathFuncs.MULTIPLY, i) );
-                                        break;
-                                    }
-                                    case '+':
-                                    {
-                                        FunctionList.Add( (MathFuncs.PLUS, i) );
-                                        break;
-                                    }
-                                    case '-':
-                                    {
-                                        FunctionList.Add( (MathFuncs.MINUS, i) );
-                                        break;
-                                    }
-
-                                    default:
-                                    break;
-                                }
-                            }
-                            float[] Values = new float[ FunctionList.Count + 1 ];
-                            int StringIndex = 0;
-                            for ( int i = 0; i < Values.Length; ++i )
-                            {
-                                int StringNextIndex = i != Values.Length - 1 ? FunctionList[ i ].Item2 : Expression.Length;
-
-                                if ( StringNextIndex == StringIndex )
-                                    throw new InvalidInputException( "Two operators next to each other, or one at the start" );
-
-                                string StringValue = Expression.Substring( StringIndex, StringNextIndex - StringIndex );
-                                if ( !float.TryParse( StringValue, out Values[ i ] ) )
-                                    throw new InvalidInputException( "Couldn't parse to float" );
-
-                                StringIndex = StringNextIndex + 1;
-                            }
-                            float ret = Values[ 0 ];
-                            for ( int i = 1; i < Values.Length; ++i )
-                            {
-                                float Val = Values[ i ];
-                                switch ( FunctionList[ i - 1 ].Item1 )
-                                {
-                                    case MathFuncs.PLUS:
-                                    {
-                                        ret += Val;
-                                        break;
-                                    }
-                                    case MathFuncs.MINUS:
-                                    {
-                                        ret -= Val;
-                                        break;
-                                    }
-                                    case MathFuncs.MULTIPLY:
-                                    {
-                                        ret *= Val;
-                                        break;
-                                    }
-                                    case MathFuncs.DIVIDE:
-                                    {
-                                        ret /= Val;
-                                        break;
-                                    }
-
-                                    case MathFuncs.NONE:
-                                    {
-                                        Assert( false );
-                                        break;
-                                    }
-
-                                    default:
-                                    {
-                                        Assert( false, "Function not in list" );
-                                        break;
-                                    }
-                                }
-                            }
-                            FunctionList.Clear();
-                            Calc.Text = ret.ToString();
-                            CurrentIndex = Calc.Text.Length;
+                            Calc.Text = EvaluateString( Calc.Text ).ToString( CultureInfo.CurrentCulture );
                         }
                         catch ( InvalidInputException ex )
                         {
                             Calc.Text = "Invalid Input";
                             _ = MessageBox.Show( ex.StackTrace, ex.Message );
                         }
+                        catch ( ArgumentOutOfRangeException ex )
+                        {
+                            Calc.Text = "Please check your parenthases";
+                            _ = MessageBox.Show( ex.StackTrace, ex.Message );
+                        }
                         break;
                     }
                     case "Delete":
                     {
-                        if ( CurrentIndex <= 0 )
-                            break;
-
-                        Calc.Text = Calc.Text.Remove( CurrentIndex );
-                        --CurrentIndex;
-                        break;
-                    }
-                    case "Left":
-                    {
-                        if ( CurrentIndex > 0 )
-                            --CurrentIndex;
-                        break;
-                    }
-                    case "Right":
-                    {
-                        if ( CurrentIndex < Calc.Text.Length )
-                            ++CurrentIndex;
+                        Calc.Text = Calc.Text[ ..^1 ];
                         break;
                     }
                     default:
@@ -190,7 +89,153 @@ namespace Calculator
             }
         }
 
+        private float EvaluateString( string Expression )
+        {
+            Expression = Expression.Replace( "pi", MathF.PI.ToString( CultureInfo.CurrentCulture ), StringComparison.CurrentCultureIgnoreCase );
+            Expression = Expression.Replace( "e", MathF.E.ToString( CultureInfo.CurrentCulture ), StringComparison.CurrentCulture );
+            Expression = Expression.Replace( ")(", ")*(", StringComparison.CurrentCulture );
 
+            //if there are parenthases, find the maximum enclosed section, and get it's value
+            //while loop for parallel parenthases like "(2+3)*(4+5)
+            while ( Expression.Contains( '(' ) || Expression.Contains( ')' ) )
+            {
+                int StartPerimIndex = Expression.IndexOf( '(' );
+                int LeftPerims = 0;
+                int RightPerims = 0;
+                int EndPerimIndex = 0;
+                for ( int i = StartPerimIndex; i < Expression.Length; ++i )
+                {
+                    if ( Expression[ i ] == '(' )
+                        ++LeftPerims;
+                    if ( Expression[ i ] == ')' )
+                        ++RightPerims;
+
+                    if ( LeftPerims == RightPerims )
+                    {
+                        EndPerimIndex = i;
+                        break;
+                    }
+                }
+                Assert( EndPerimIndex != 0 );
+                string EnclosedSectionString = Expression[ (StartPerimIndex + 1)..EndPerimIndex ];
+                //condense it down to a single number represented as a string
+                string EnclosedSectionValue = EvaluateString( EnclosedSectionString ).ToString( CultureInfo.CurrentCulture );
+                Expression = Expression[ 0..StartPerimIndex ] + EnclosedSectionValue + Expression[ ( EndPerimIndex + 1 ).. ];
+            }
+
+
+            //get the functions from the long string
+            List<(MathFuncs, int)> FunctionList = new();
+            for ( int i = 0; i < Expression.Length; ++i )
+            {
+                switch ( Expression[ i ] )
+                {
+                    case '/':
+                    {
+                        FunctionList.Add( (MathFuncs.DIVIDE, i) );
+                        break;
+                    }
+                    case '*':
+                    {
+                        FunctionList.Add( (MathFuncs.MULTIPLY, i) );
+                        break;
+                    }
+                    case '+':
+                    {
+                        //+ is also used in cases like "2E+10", so check for that
+                        if ( Expression[ i - 1 ] == 'E' )
+                            break;
+
+                        FunctionList.Add( (MathFuncs.PLUS, i) );
+                        break;
+                    }
+                    case '-':
+                    {
+                        //see above
+                        if ( Expression[ i - 1 ] == 'E' )
+                            break;
+
+                        FunctionList.Add( (MathFuncs.MINUS, i) );
+                        break;
+                    }
+
+                    default:
+                    break;
+                }
+            }
+
+            //turn the long string into values
+            int ValueCount = FunctionList.Count + 1;
+            List<(float, int)> Values = new( ValueCount );
+            int StringIndex = 0;
+            for ( int i = 0; i < ValueCount; ++i )
+            {
+                int StringNextIndex = i != ValueCount - 1 ? FunctionList[ i ].Item2 : Expression.Length;
+
+                if ( StringNextIndex == StringIndex )
+                    throw new InvalidInputException( "Two operators next to each other, or one at the start" );
+
+                string StringValue = Expression[ StringIndex..StringNextIndex ];
+                if ( float.TryParse( StringValue, out float f ) )
+                    Values.Add( (f, StringIndex) );
+                else
+                    throw new InvalidInputException( "Couldn't parse to float" );
+
+                StringIndex = StringNextIndex + 1;
+            }
+
+            //if there are parenthases, get the value inside of them before proceeding
+
+
+            //do order of operations by condensing values by their MathFuncs enum top to bottom
+            int MathFuncNum = Enum.GetValues( typeof( MathFuncs ) ).Length;
+            for ( int i = 0; i < MathFuncNum; ++i )
+            {
+                for ( int j = FunctionList.Count; --j >= 0; )
+                {
+                    MathFuncs f = (MathFuncs) ( 1 << i );
+                    if ( FunctionList[ j ].Item1 == f )
+                    {
+                        switch ( f )
+                        {
+                            case MathFuncs.MULTIPLY:
+                            {
+                                Values[ j ] = (Values[ j ].Item1 * Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values.RemoveAt( j + 1 );
+                                break;
+                            }
+                            case MathFuncs.DIVIDE:
+                            {
+                                Values[ j ] = (Values[ j ].Item1 / Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values.RemoveAt( j + 1 );
+                                break;
+                            }
+                            case MathFuncs.PLUS:
+                            {
+                                Values[ j ] = (Values[ j ].Item1 + Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values.RemoveAt( j + 1 );
+                                break;
+                            }
+                            case MathFuncs.MINUS:
+                            {
+                                Values[ j ] = (Values[ j ].Item1 - Values[ j + 1 ].Item1, Values[ j ].Item2);
+                                Values.RemoveAt( j + 1 );
+                                break;
+                            }
+
+                            default:
+                            {
+                                throw new MissingMemberException( "Catastrophic failure: I don't even know what could possibly cause this" );
+                            }
+                        }
+                        FunctionList.RemoveAt( j );
+                    }
+                }
+            }
+
+            Assert( Values.Count == 1 );
+            return Values[ 0 ].Item1;
+        }
 
         private void FuncButton( object sender, RoutedEventArgs e )
         {
@@ -200,32 +245,37 @@ namespace Calculator
                 {
                     case "pls":
                     {
-                        Calc.Text = Calc.Text.Insert( CurrentIndex, "+" );
-                        ++CurrentIndex;
+                        Calc.Text += "+";
                         break;
                     }
                     case "sbt":
                     {
-                        Calc.Text = Calc.Text.Insert( CurrentIndex, "-" );
-                        ++CurrentIndex;
+                        Calc.Text += "-";
                         break;
                     }
                     case "mult":
                     {
-                        Calc.Text = Calc.Text.Insert( CurrentIndex, "*" );
-                        ++CurrentIndex;
+                        Calc.Text += "*";
                         break;
                     }
                     case "div":
                     {
-                        Calc.Text = Calc.Text.Insert( CurrentIndex, "/" );
-                        ++CurrentIndex;
+                        Calc.Text += "/";
                         break;
                     }
                     case "dot":
                     {
-                        Calc.Text = Calc.Text.Insert( CurrentIndex, "." );
-                        ++CurrentIndex;
+                        Calc.Text += ".";
+                        break;
+                    }
+                    case "leftperim":
+                    {
+                        Calc.Text += "(";
+                        break;
+                    }
+                    case "rightperim":
+                    {
+                        Calc.Text += ")";
                         break;
                     }
                     default:
